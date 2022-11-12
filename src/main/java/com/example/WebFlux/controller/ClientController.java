e09280472901f4929d0cc8bfa466a7ec4fb29af5
Fredy <fredy.huachaca21@gmail.com>
2022-11-12 00:22:09 -0500

    Add ResponseEntity Controller

--- src/main/java/com/example/WebFlux/controller/ClientController.java
+++ src/main/java/com/example/WebFlux/controller/ClientController.java
@@ -4,9 +4,9 @@ import com.example.WebFlux.model.Client;
 import com.example.WebFlux.service.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,22 +18,35 @@ public class ClientController {
     private IClientService service;
 
     @GetMapping()
-    public Flux<Client> listar(){
-        return service.listar();
+    public Mono<ResponseEntity<Flux<Client>>> listar(){
+        Flux<Client> clientFlux = service.listar();
+        return Mono.just(ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(clientFlux))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
     }
 
     @PostMapping
-    public Mono<Client> registrar(@RequestBody Client client){
-        return service.registrar(client);
+    public Mono<ResponseEntity<Client>> registrar(@RequestBody Client client){
+        return service.registrar(client)
+                .map(newClient ->
+                        ResponseEntity.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(newClient))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
     }
 
     @GetMapping("/{id}")
-    public  Mono<Client> buscarPorId(@PathVariable(value = "id") String id){
-        return service.listarPorId(id);
+public Mono<ResponseEntity<Client>> buscarPorId(@PathVariable(value = "id") String id){
+        return service.listarPorId(id)
+                .map(client -> ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(client))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
     }
 
     @PutMapping("/{id}")
-    public Mono<Client> actualizar(@RequestBody Client client, @PathVariable("id") String id ){
+    public Mono<ResponseEntity<Client>> actualizar(@RequestBody Client client, @PathVariable("id") String id ){
         Mono<Client> clientMono = Mono.just(client);
         Mono<Client> clientDB = service.listarPorId(id);
 
@@ -45,13 +58,22 @@ public class ClientController {
                     return db;
                 })
                 .flatMap(x -> service.modificar(x))
-                .defaultIfEmpty(client);
+                .map(cli ->
+                    ResponseEntity.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(cli)
+                )
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NO_CONTENT));
 
     }
 
     @DeleteMapping("/{id}")
-    public Mono<Void> eliminarClient(@PathVariable(value = "id") String id){
-        return service.eliminar(id);
+    public Mono<ResponseEntity<Void>> eliminarClient(@PathVariable(value = "id") String id){
+        return service.eliminar(id)
+                .map(cliDelete -> ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(cliDelete))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
     }
 
 }
